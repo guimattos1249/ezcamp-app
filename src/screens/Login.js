91e6e615dc10944d5ae79633362aa8adcf3b51c4
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-18 15:46:18 -0300

    Added Screen Login

---
+++ src/screens/Login.js
@@ -0,0 +1,62 @@
+import React, {useCallback, useState} from 'react';
+import {Image, TextInput, TouchableOpacity, View, Text} from 'react-native';
+
+import logo from './src/assets/images/logo.png';
+
+export default () => 
+{
+  const [username, setUsername] = useState('');
+
+  const onSubmit = useCallback(() => { 
+    //TODO try to auth
+    //TODO navigate to main screen
+   }, []);
+
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: '#f5f5f5',
+      paddingHorizontal: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Image source={logo} />
+      <TextInput 
+        autoCapitalize="none" 
+        placeholder="Digite seu Usuário no GitHub"
+        value={username}
+        onChangeText={setUsername}
+        style={{
+            marginTop: 20,
+            paddingHorizontal: 15,
+            alignSelf: 'stretch',
+            height: 45,
+            backgroundColor: '#fff',
+            borderWidth: 1,
+            borderColor: '#ddd',
+            borderRadius: 4,
+          }}
+      />
+      <TouchableOpacity 
+        onPress={onSubmit}
+        style={{
+          marginTop: 10,
+          height: 45,
+          alignSelf: 'stretch',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#DF4723',
+          borderRadius: 4,
+        }}>
+          <Text 
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              fontSize: 16,
+            }}>
+              Sing In
+          </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
